ebde40f4d8456f0c8c2ca8a9d0d9196640dd7e66
Lupo Montero <lupo@e-noise.com>
2012-04-13 17:20:50 -0500

    Fixed typo and whitespace...

--- cpphello.cpp
+++ cpphello.cpp
@@ -9,8 +9,7 @@ static Handle<Value> foo(const Arguments& args)
   return String::New("Hello World");
 }
 
-
-extern "C" void initd(Handle<Object> target)
+extern "C" void init(Handle<Object> target)
 {
   NODE_SET_METHOD(target, "foo", foo);
 }
