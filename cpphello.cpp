546f90107ebad9aa5cc17d7b4d9b5a9b6223ddc6
Lupo Montero <lupomontero@gmail.com>
2015-09-16 10:32:46 -0500

    refactor for node v4.0.0

--- cpphello.cpp
+++ cpphello.cpp
@@ -1,16 +1,24 @@
-#include <v8.h>
 #include <node.h>
 
-using namespace v8;
+namespace cpphello {
 
-Handle<Value> foo(const Arguments& args) {
-  HandleScope scope;
-  return scope.Close(String::New("Hello World"));
+  using v8::FunctionCallbackInfo;
+  using v8::Isolate;
+  using v8::Local;
+  using v8::Object;
+  using v8::String;
+  using v8::Value;
+
+  void Foo(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+    args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Hello World"));
+  }
+
+  void Init(Local<Object> exports) {
+    NODE_SET_METHOD(exports, "foo", Foo);
+  }
+
+  NODE_MODULE(cpphello, Init)
+
 }
-
-void init(Handle<Object> exports) {
-  NODE_SET_METHOD(exports, "foo", foo);
-}
-
-NODE_MODULE(cpphello, init)
 
